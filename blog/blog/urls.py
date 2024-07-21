a2cd6c51eeca872ff6165382e3802b91ab31494c
prajwalhaniya <prajwalhaniya@gmail.com>
2024-07-21 23:10:14 +0530

    ch7

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import post
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView
+)
 
 urlpatterns = [
-    path("", post, name="post")
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("", BlogListView.as_view(), name="home")
 ]
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
-# Create your views here.
-def post(request):
-    # posts = Post.objects.all()
-    return render(request, "base.html")
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_create.html"
+    fields = ["title", "author", "body"]
