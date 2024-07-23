241d33435131a2eb27abe276edab357bf81255a8
prajwalhaniya <prajwalhaniya@gmail.com>
2024-07-23 23:55:03 +0530

    Add update and delete functionality for the blog

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class BlogListView(ListView):
@@ -14,3 +15,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_create.html"
     fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
