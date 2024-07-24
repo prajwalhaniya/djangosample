55c130800bdd1f3e4cd844845f4acc043bd9ad7e
prajwalhaniya <prajwalhaniya@gmail.com>
2024-07-24 23:06:28 +0530

    add login, logout and signup feature

--- blog/django_project/settings.py
+++ blog/django_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog"
+    "blog",
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 STATIC_URL = 'static/'
 STATICFILES_DIR = [BASE_DIR / "static"]
